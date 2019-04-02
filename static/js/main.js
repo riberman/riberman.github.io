d24a464288ba19e254a8953a497ff74a919d35c6
Patrick Ferro Ribeiro <patrick.ferro.ribeiro@gmail.com>
2019-04-01 21:31:51 -0300

    JS button back to top

--- static/js/main.js
+++ static/js/main.js
@@ -1,22 +1,15 @@
 (function($){
 	$(document).ready(function(){
-		//Empty space. Fill this with your site specific JS code
+		$(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#backTop').fadeIn();
+        } else {
+            $('#backTop').fadeOut();
+        }
+    });
+    $('#backTop').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
 	});
 })(jQuery);
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("backTop").style.display = "block";
-  } else {
-    document.getElementById("backTop").style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
